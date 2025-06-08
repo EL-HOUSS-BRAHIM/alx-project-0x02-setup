3acde7055264d96b30cbb7e7dfcd2062e9cff02f
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-06-09 00:07:32 +0100

    pushing index updated files to the repo

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,16 @@
+import Layout from '@/components/layout/layout'
+import React from 'react'
+
+const index = () => {
+  return (
+    <Layout>  
+      <h1>
+    welcome to this page
+  </h1>
+    </Layout>
+      
+    
+  )
+}
+
+export default index
