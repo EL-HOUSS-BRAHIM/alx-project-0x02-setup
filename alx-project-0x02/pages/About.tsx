4d50c35872d07801dcba7968aeee50c391c110da
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-06-09 00:07:20 +0100

    pushing About updated files to the repo

---
+++ alx-project-0x02/pages/About.tsx
@@ -0,0 +1,17 @@
+import Button from '@/components/common/Button'
+import React from 'react'
+import Header from '@/components/layout/Header'
+
+<Header/>
+
+const about = () => {
+  return (
+    <>
+    <Button size="small" shape="round-sm" />
+    <Button size="medium" shape="round-md" />
+    <Button size="large" shape="round-full" />
+    </>
+  )
+}
+
+export default about
